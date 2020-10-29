301ab64410b636b058f3176b6d93bb93711b897d
Pappouth <lammnslouis.rs@gmail.com>
2020-10-29 12:12:09 +0100

    modif

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace FizzBuzz
     {
         static void Main(string[] args)
         {
-            for (int i = 0; i <= 100; i++)
+            for (int i = 1; i <= 100; i++)
             {
                 if (i % 3 == 0 && i % 5 == 0)
                     Console.WriteLine("FizzBuzz");
